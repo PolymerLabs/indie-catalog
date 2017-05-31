a5abac0ef43ff471bf037af8676d98410b3aba47
Monica Dinculescu <noms@google.com>
2017-05-31 14:20:33 -0700

    add docs

--- gulpfile.js
+++ gulpfile.js
@@ -21,30 +21,37 @@ gulp.task('checkout', function() {
     let repoName = repo;
     repos.push(path);
 
+    // Step 1. Clone the element.
     git.clone(packages[repo].git, {args: '--depth 1 -- ' + path}, function (err) {
       if (err) {
         console.log(err);
         return;
       }
-      
+
+      // Step 2. Delete the .git from it. Be very careful not to
+      // Delete your own .git repo 🙄.
       del([path + '/.git']);
       del([path + '/.gitignore']);
 
+      // Step 3. bower install the element's dependencies.
       console.log('Running bower install in ' + path);
       bower({cwd: path, verbosity: 1}).on('end', function() {
-        // Copy the element in the bower_components, so the demo works.
-        gulp.src(path + '/**').pipe(gulp.dest(`${path}/bower_components/${repoName}`));
+        // Step 4. Copy the element in its bower_components, so that
+        // the demo works.
+        gulp.src(path + '/**').pipe(gulp.dest(`${path}/bower_components/${repoName}`));g
       });
     });
   }
 });
 
-gulp.task('bower-old', function() {
-  for (var i = 0; i < repos.length; i++) {
-    bower({directory: repos[i] + '/bower_components', cwd: repos[i], verbosity: 2});
-  }
-});
+// This does't work
+// gulp.task('bower-old', function() {
+//   for (var i = 0; i < repos.length; i++) {
+//     bower({directory: repos[i] + '/bower_components', cwd: repos[i], verbosity: 2});
+//   }
+// });
 
+// And this isn't ready yet
 gulp.task('bower', function() {
   console.log('sigh');
 });
