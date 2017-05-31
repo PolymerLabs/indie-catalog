474443fd29782069a9ed96db1d8d92a823c65b98
Monica Dinculescu <noms@google.com>
2017-05-30 20:44:10 -0700

    do some inneffective gulping

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let gulp = require('gulp');
+let fs = require('fs');
+let del = require('del');
+let runSequence = require('run-sequence');
+let git = require('gulp-git');
+let bower = require('gulp-bower');
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+let repos = [];
+
+gulp.task('checkout', function() {
+  let json = JSON.parse(fs.readFileSync(__dirname + '/catalog.json'));
+  let packages = json.packages;
+  for (var repo in packages) {
+    let path = __dirname + '/dist/' + repo;
+    repos.push(path);
+    git.clone(packages[repo], {args: path}, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Running bower install in ' + path);
+      bower({cwd: path, verbosity: 1});
+    });
+  }
+});
+
+gulp.task('bower-old', function() {
+  for (var i = 0; i < repos.length; i++) {
+    bower({directory: repos[i] + '/bower_components', cwd: repos[i], verbosity: 2});
+  }
+});
+
+gulp.task('bower', function() {
+  console.log('sigh');
+});
+
+gulp.task('default', function(done) {
+  runSequence('clean', 'checkout', 'bower');
+});
